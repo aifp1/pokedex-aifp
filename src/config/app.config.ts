f8766b6b7609adbdb39a70b27118e723b2df5ef5
Andres Fuentes <fuentesandres31@gmail.com>
2023-01-27 20:38:02 -0300

    Solucion DB 2

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfiguration = () => ({
     enviroment: process.env.NODE_ENV || 'dev',
-    mongodb: process.env.MONGODB,
+    mongodb: process.env.MONGODB || 'mongodb+srv://pokemondb_user:c6GkM2d9objMzAvV@cluster0.hk9ovgn.mongodb.net/?retryWrites=true&w=majority',
     port: process.env.PORT || 3000,
     defaul_limit: +process.env.DEFAULT_LIMIT || 5,
 })
